cce0b5dc901464ba27c9d6c39656ac816c0aa6a9
Hoonah-Robotics <73606540+Hoonah-Robotics@users.noreply.github.com>
2020-11-01 18:05:07 -0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,39 @@
 radio.onReceivedValue(function (name, value) {
-    comment.comment("Sense: accelerometer on driver station")
+    comment.comment("SENSE: read accelerometer on driver station")
     if (name == "y") {
         throttle = value
     } else if (name == "x") {
         turn = value
     }
 })
-let powerRight = 0
-let powerLeft = 0
+let scalarRight = 0
+let vectorRight = 0
+let scalarLeft = 0
+let vectorLeft = 0
 let turn = 0
 let throttle = 0
 comment.comment("same radio group as driver station")
 radio.setGroup(0)
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    comment.comment("Think: calculate drive power")
-    powerLeft = throttle + turn
-    powerRight = throttle - turn
-    if (powerLeft > 0) {
-        powerLeft = Math.map(powerLeft, 0, 40, 0, 255)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, powerLeft)
-    } else if (powerLeft < 0) {
-        powerLeft = Math.map(Math.abs(powerLeft), 0, 40, 0, 255)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, powerLeft)
+    comment.comment("THINK: calculate speed and direction for each motor")
+    comment.comment("Scale motor power from 0 to 255")
+    vectorLeft = throttle + turn
+    scalarLeft = Math.map(Math.abs(vectorLeft), 0, 40, 0, 255)
+    vectorRight = throttle - turn
+    scalarRight = Math.map(Math.abs(vectorRight), 0, 40, 0, 255)
+    comment.comment("ACT: send speed and direction to motors ")
+    if (vectorLeft > 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, scalarLeft)
+    } else if (vectorLeft < 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, scalarLeft)
     } else {
         maqueen.motorStop(maqueen.Motors.M1)
     }
-    if (powerRight > 0) {
-        powerRight = Math.map(powerRight, 0, 40, 0, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, powerRight)
-    } else if (powerRight < 0) {
-        powerRight = Math.map(Math.abs(powerRight), 0, 40, 0, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, powerRight)
+    if (vectorRight > 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, scalarRight)
+    } else if (vectorRight < 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, scalarRight)
     } else {
         maqueen.motorStop(maqueen.Motors.M2)
     }
